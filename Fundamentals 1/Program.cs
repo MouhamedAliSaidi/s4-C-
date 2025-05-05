3fc79bf8778776055ffc254d894d1c57d39b45ca
Dali <medalisaidi009@gmail.com>
2025-05-05 19:19:46 +0100

    Fundamentals 1

---
+++ Fundamentals 1/Program.cs
@@ -0,0 +1,49 @@
+﻿for (int i = 1 ; i<255 ; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+
+Random rand = new Random();
+int sum = 0;
+for (int i = 10 ; i<20 ; i+= 2)
+{
+    int randomNum = rand.Next(10,20);
+    System.Console.WriteLine(randomNum);
+    sum += randomNum;
+}
+    System.Console.WriteLine(sum);
+
+
+for (int i = 0 ; i<= 100; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        System.Console.WriteLine(i);
+    } 
+}
+for (int i = 0 ; i<= 100; i++)
+{
+    if (i % 3 == 0 )
+    {
+        System.Console.WriteLine("fizz"+i);
+    } 
+}
+for (int i = 0 ; i<= 100; i++)
+{
+    if (i % 5 == 0 )
+    {
+        System.Console.WriteLine("buzz"+i);
+    } 
+}
+
+for (int i = 0 ; i<= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.WriteLine("fizzbuzz"+i);
+    } 
+}
+
+
